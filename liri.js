ce5a7ee7d7c25d46e7e1b872c4fead74fb2d3e42
MEGA-MAN-EXE\wolfe <mxv647@med.miami.edu>
2018-04-02 21:16:49 -0400

    added OMDB functionality

--- liri.js
+++ liri.js
@@ -4,8 +4,7 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 var inputString = process.argv;
-//var operand = inputString[2];
-// var searchTerm = inputString[3];
+
 var twitter = require("twitter");
 var Spotify = require("node-spotify-api");
 var request = require('request');
@@ -61,14 +60,40 @@ clientS.search({ type: 'track', query: searchTerm, limit: 2 }, function(err, dat
     console.log(data);
   });
 };
+
+var getMeMovie = function(movieName) {
+
+  if (movieName === undefined) {
+    movieName = 'Mr Nobody';
+  }
+
+  var queryURL = "https://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+  request(queryURL, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = [];
+      var jsonData = JSON.parse(body);
+
+      data.push({
+      'Title: ' : jsonData.Title,
+      'Year: ' : jsonData.Year,
+      'IMDB Rating: ' :jsonData.imdbRating,
+      'Rotten Tomatoes Ratings:' : jsonData.Ratings[1].Value,
+      'Country: ' : jsonData.Country,
+      'Language: ' : jsonData.Language, 
+      'Plot: ' : jsonData.Plot,
+      'Actors: ' : jsonData.Actors,
+  });
+     console.log(data) 
+     // console.log(JSON.parse(body));||<-was used to read the JSON to determine how to extract needed data
+}
+  });
+
+}
+
+//Function for getting movie information, caseData = move-this <movie name here>
 //Main Processes
 //---------------------------------------------------
-// if (operand === "my-tweets"){
-// 	getTweets();
-// }
-// else if (operand === "spotify-this-song"){
-//   getMeSpotify();
-// }
 var pick = function(caseData, functionData) {
   switch (caseData) {
     case 'my-tweets':
@@ -77,14 +102,14 @@ var pick = function(caseData, functionData) {
     case 'spotify-this-song':
       getMeSpotify(functionData);
       break;
-    // case 'movie-this':
-    //   getMeMovie(functionData);
-    //   break;
-    // case 'do-what-it-says':
-    //   doWhatItSays();
-    //   break;
-    // default:
-    //   console.log('LIRI doesn\'t know that');
+    case 'movie-this':
+      getMeMovie(functionData);
+      break;
+    case 'do-what-it-says':
+      doWhatItSays();
+      break;
+    default:
+      console.log('LIRI doesn\'t know that');
   }
 }
 //run this on load of js file
@@ -94,53 +119,6 @@ var runThis = function(argOne, argTwo) {
 
 runThis(process.argv[2], process.argv[3]);
 //---------------------------------------------------
-
-
-// TWO `node liri.js spotify-this-song '<song name here>'`
-
-//      * Artist(s)
-//      * The song's name
-//      * A preview link of the song from Spotify
-//      * The album that the song is from
-
-//    * If no song is provided then your program will default to "The Sign" by Ace of Base.
-   
-//    * You will utilize the [node-spotify-api](https://www.npmjs.com/package/node-spotify-api) package in order to retrieve song information from the Spotify API.
-   
-//    * Like the Twitter API, the Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a **client id** and **client secret**:
-
-//    * Step One: Visit <https://developer.spotify.com/my-applications/#!/>
-   
-//    * Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
-
-//    * Step Three: Once logged in, navigate to <https://developer.spotify.com/my-applications/#!/applications/create> to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
-
-//    * Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the [node-spotify-api package](https://www.npmjs.com/package/node-spotify-api).
-
-
-// THREE    * `movie-this`
-//     `node liri.js movie-this '<movie name here>'`
-
-//    * This will output the following information to your terminal/bash window:
-
-//      ```
-//        * Title of the movie.
-//        * Year the movie came out.
-//        * IMDB Rating of the movie.
-//        * Rotten Tomatoes Rating of the movie.
-//        * Country where the movie was produced.
-//        * Language of the movie.
-//        * Plot of the movie.
-//        * Actors in the movie.
-//      ```
-
-//    * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-     
-//      * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
-     
-//      * It's on Netflix!
-   
-//    * You'll use the request package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use `trilogy`.
 
 // FOUR    * `do-what-it-says`
 //     `node liri.js do-what-it-says`
